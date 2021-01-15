dbe2779fdd20bb090eedc48c8a18b8ebe25ddd42
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-01-15 14:17:45 +0900

    BOJ - CSS if문

---
+++ BOJ CSS/CS2753_윤년.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace CStest2753
+{
+    class CStest2753
+    {
+        static void Main(string[] args)
+        {
+            string year = ReadLine();
+            int a = int.Parse(year);
+
+            if (a % 4 == 0 && a % 100 != 0)
+                WriteLine("1");
+            else if (a % 400 == 0)
+                WriteLine("1");
+            else
+                WriteLine("0");
+        }
+    }
+}
