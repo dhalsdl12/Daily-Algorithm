a1357de56ff934beeb1530010bd1595fe2debd16
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-01-15 14:22:09 +0900

    BOJ - CSS 1110 (더하기 싸이클)

---
+++ BOJ CSS/CS1110_더하기_싸이클.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace CStest1110
+{
+    class CStest1110
+    {
+        static void Main(string[] args)
+        {
+            string num = ReadLine();
+            int cnt = 0;
+            int add;
+            if (int.Parse(num) <= 9)
+                num = num + 0;
+
+            string origin = num;
+            while(true)
+            {
+                cnt++;
+                int a = int.Parse(num) / 10;
+                int b = int.Parse(num) % 10;
+
+                add = a + b;
+
+                num = b.ToString() + (add % 10).ToString();
+
+                if (int.Parse(origin) == int.Parse(num))
+                    break;
+            }
+            WriteLine(cnt);
+        }
+    }
+}
