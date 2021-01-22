6d6fe9262c9c7b65a045310467c7c23f09fe3ed2
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-01-22 10:21:37 +0900

    BOJ - Java

---
+++ BOJ Java/JV4673.java
@@ -0,0 +1,17 @@
+package JVtest4673;
+
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter (System.out));
+		BufferedReader A = new BufferedReader(new InputStreamReader (System.in));
+		
+		int arr[] = new int[0];
+		for(int i = 1; i < 100; i++)
+		{
+			
+		}
+	}
+
+}
