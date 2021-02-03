7a33126e4e695ab5b58b9a888484469f8a776aeb
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-02-03 22:55:00 +0900

    BOJ - Python 1629 (곱셈)

--- BOJ Python/PY1629_곱셈.py
+++ BOJ Python/PY1629_곱셈.py
@@ -1,14 +1,14 @@
 import sys
 
-def power(a, b):
-    if b== 1:
-        return a%c
-    else:
-        temp = power(a, b//2)
-        if b%2 == 0:
-            return temp*temp%c
-        else:
-            return temp*temp*a%c
+def multi(a, b):
+    if(b == 1):
+        return a
+    if(b == 0):
+        return 1
+    elif(b%2 == 0):
+        return multi(a, b//2)**2%c
+    elif(b%2 == 1):
+        return (multi(a,b-1))*a
 
 a,b,c = map(int, sys.stdin.readline().split())
-print(power(a,b))
+print(multi(a, b)%c)
