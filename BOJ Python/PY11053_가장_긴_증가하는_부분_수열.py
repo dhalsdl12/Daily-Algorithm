451cc6ef72a6e25c17317160b97758887ebd5d3c
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-06-01 11:59:32 +0900

    BOJ - Python 가장 긴 증가하는 부분 수열

---
+++ BOJ Python/PY11053_가장_긴_증가하는_부분_수열.py
@@ -0,0 +1,13 @@
+from bisect import bisect_left
+
+num = int(input())
+a = list(map(int, input().split()))
+stack = [0]
+
+for i in a:
+    if stack[-1] < i:
+        stack.append(i)
+    else:
+        stack[bisect_left(stack, i)] = i
+
+print(len(stack) - 1)
