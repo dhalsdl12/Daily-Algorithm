2576dff83f659b9d9ea761e75ec1db1bf6dfd7eb
dhalsdl12 <dhalsdl12@gmail.com>
2022-12-22 15:16:30 +0900

    별찍기

--- BOJ Python/bronze3/PY2446_별_찍기9.py
+++ BOJ Python/bronze3/PY2446_별_찍기9.py
@@ -2,7 +2,7 @@ n = int(input())
 
 for i in range(n):
     k = n - i - 1
-    print(' ' * k + '*' * ((i + 1) * 2 - 1))
+    print(' ' * i + '*' * (k * 2 + 1))
 for i in range(n - 2, -1, -1):
     k = n - i - 1
-    print(' ' * k + '*' * ((i + 1) * 2 - 1))
+    print(' ' * i + '*' * (k * 2 + 1))
