946474e80ebd30524709bbfb6609ef4e8a587da4
dhalsdl12 <dhalsdl12@gmail.com>
2023-01-20 15:00:37 +0900

    boj 230120

---
+++ BOJ/BOJ Python/silver3/PY1904_01타일.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+dp = [0 for i in range(n + 1)]
+if n == 1:
+    exit()
+elif n == 2:
+    print(2)
+else:
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+
+    print(dp[-1])
