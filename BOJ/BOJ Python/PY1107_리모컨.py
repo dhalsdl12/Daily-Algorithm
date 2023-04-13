e74ff707ae3bb53b05c61561131b679eeb67e390
dhalsdl12 <dhalsdl12@gmail.com>
2023-04-13 12:52:06 +0900

    230413 1107

---
+++ BOJ/BOJ Python/PY1107_리모컨.py
@@ -0,0 +1,17 @@
+n = int(input())
+m = int(input())
+arr = list(map(int, input().split()))
+
+min_cnt = abs(100 - n)
+
+for i in range(1000001):
+    number = str(i)
+
+    for j in range(len(number)):
+        if int(number[j]) in arr:
+            break
+        elif j == len(number) - 1:
+            min_cnt = min(min_cnt, abs(int(number) - n) + len(number))
+
+
+print(min_cnt)
