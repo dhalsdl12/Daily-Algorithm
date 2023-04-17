ca162fa51809f2b1e217a9dc77e6b55c73c559b7
dhalsdl12 <dhalsdl12@gmail.com>
2023-04-17 16:56:31 +0900

    230417

---
+++ BOJ/BOJ Python/PY27889_특별한_학교_이름.py
@@ -0,0 +1,10 @@
+name = input()
+
+if name == 'NLCS':
+    print('North London Collegiate School')
+if name == 'BHA':
+    print('Branksome Hall Asia')
+if name == 'KIS':
+    print('Korea International School')
+if name == 'SJA':
+    print('St. Johnsbury Academy')
