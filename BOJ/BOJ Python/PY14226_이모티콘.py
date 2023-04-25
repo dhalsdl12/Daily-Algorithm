02f460dfe7849702495e2a0960d88f29fc878fc2
dhalsdl12 <dhalsdl12@gmail.com>
2023-04-25 14:27:51 +0900

    230425

--- BOJ/BOJ Python/PY14226_이모티콘.py
+++ BOJ/BOJ Python/PY14226_이모티콘.py
@@ -6,20 +6,19 @@ def bfs():
     queue.append((1, 0, 0))
     visit[1][0] = 1
     while queue:
-        inputs, clip, time = queue.popleft()
-        if inputs == n:
+        s, clip, time = queue.popleft()
+        if s == n:
             return time
         
-        tmp = inputs + clip
-        if visit[inputs][inputs] == 0:
-            visit[inputs][inputs] = 1
-            queue.append((inputs, inputs, time + 1))
-        if tmp <= n and visit[tmp][clip] == 0:
-            visit[tmp][clip] = 1
-            queue.append((tmp, clip, time + 1))
-        if inputs - 1 >= 0 and visit[inputs - 1][clip] == 0:
-            visit[inputs - 1][clip] = 1
-            queue.append((inputs - 1, clip, time + 1))
+        if visit[s][s] == 0:
+            visit[s][s] = 1
+            queue.append((s, s, time + 1))
+        if s + clip <= n and visit[s + clip][clip] == 0:
+            visit[s + clip][clip] = 1
+            queue.append((s + clip, clip, time + 1))
+        if s - 1 >= 0 and visit[s - 1][clip] == 0:
+            visit[s - 1][clip] = 1
+            queue.append((s - 1, clip, time + 1))
 
 
 n = int(input())
