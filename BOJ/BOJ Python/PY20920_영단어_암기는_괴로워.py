15e6f2b175d4f9ea538d4f341d4cb2e1ff9c21d3
dhalsdl12 <dhalsdl12@gmail.com>
2023-05-26 12:57:48 +0900

    python 20920

---
+++ BOJ/BOJ Python/PY20920_영단어_암기는_괴로워.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+dic = {}
+
+for _ in range(n):
+    v = input()
+    if len(v) >= m:
+        if v not in dic:
+            dic[v] = 1
+        else:
+            dic[v] += 1
+
+s_dic = sorted(dic.items(), key = lambda x : (-x[1], -len(x[0]), x[0]))
+
+for a in s_dic:
+    print(a[0])
