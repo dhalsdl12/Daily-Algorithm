3d36d6f44e5b9798f0b5a9e7eed6e0a6548e5b81
dhalsdl12 <dhalsdl12@gmail.com>
2023-05-30 18:59:17 +0900

    230530

---
+++ BOJ/BOJ Python/PY2985_세_수.py
@@ -0,0 +1,18 @@
+a, b, c = map(int, input().split())
+
+if a + b == c:
+    print('%d+%d=%d' %( a, b, c))
+elif a - b == c:
+    print('%d-%d=%d' %( a, b, c))
+elif a * b == c:
+    print('%d*%d=%d' %( a, b, c))
+elif a // b == c:
+    print('%d/%d=%d' %( a, b, c))
+elif a == b + c:
+    print('%d=%d+%d' %( a, b, c))
+elif a == b - c:
+    print('%d=%d-%d' %( a, b, c))
+elif a == b * c:
+    print('%d=%d*%d' %( a, b, c))
+elif a == b // c:
+    print('%d=%d/%d' %( a, b, c))
