78c7757ad242506afa576521051d99876250bcf1
dhalsdl12 <dhalsdl12@gmail.com>
2023-06-16 20:51:55 +0900

    230616

---
+++ BOJ/BOJ Python/PY25704_출석_이벤트.py
@@ -0,0 +1,19 @@
+n = int(input())
+p = int(input())
+sale = []
+
+if n >= 5:
+    sale.append(500)
+if n >= 10:
+    sale.append(p // 10)
+if n >= 15:
+    sale.append(2000)
+if n >= 20:
+    sale.append(p // 4)
+
+answer = p - max(sale)
+
+if answer < 0:
+    print(0)
+else:
+    print(answer)
