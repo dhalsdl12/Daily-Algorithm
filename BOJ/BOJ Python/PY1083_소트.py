fca4915891f91fcd1dbc02d3da048df199557177
dhalsdl12 <dhalsdl12@gmail.com>
2023-07-14 15:40:24 +0900

    1083 살짝 변경

--- BOJ/BOJ Python/PY1083_소트.py
+++ BOJ/BOJ Python/PY1083_소트.py
@@ -5,9 +5,9 @@ s = int(input())
 for i in range(n):
     num = max(arr[i : min(n, i + s + 1)])
     idx = arr.index(num)
-    
-    for j in range(idx, i, -1):
-        arr[j], arr[j - 1] = arr[j - 1], arr[j]
+
+    arr.pop(idx)
+    arr.insert(i, num)
     
     s -= (idx - i)
     if s == 0:
