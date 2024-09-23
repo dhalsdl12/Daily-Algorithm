6d01fcd3f14a19cb7eeaf90e97e993df61f1f832
dhalsdl12 <dhalsdl12@gmail.com>
2024-09-24 00:14:54 +0900

    dp

---
+++ BOJ/BOJ Python/PY2193_이친수.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+if n == 1:
+    print(1)
+else:
+    dp = [0 for _ in range(n + 1)]
+    dp[1] = 1
+    dp[2] = 1
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    print(dp[n])
